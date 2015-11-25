f667f3d408dc7fafd3d210336e79ea3170284cac
Orpheus Peng <stannolite@gmail.com>
2015-11-24 20:01:33 -0500

    Added basic endpoint of the plugin.

---
+++ WaterfallGalleryLayoutPlugin.php
@@ -0,0 +1,13 @@
+<?php
+  class WaterfallGalleryLayoutPlugin extends Omeka_Plugin_AbstractPlugin {
+    protected $_filters = array('exhibit_layouts');
+
+    public function filterExhibitLayouts($layouts) {
+      $layouts['waterfall_gallery'] = array(
+        'name' => 'Waterfall Gallery Layout',
+        'description' => 'A waterfall-like exhibit gallery layout.'
+      );
+      return $layouts;
+    }
+  }
+?>
